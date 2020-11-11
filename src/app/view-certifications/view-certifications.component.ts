f07d766fd1a3faa2d6a25956b79a33964bbba59e
Kelvin Yan <kelvinyan85@gmail.com>
2020-11-11 21:47:32 +0800

    .

--- src/app/view-certifications/view-certifications.component.ts
+++ src/app/view-certifications/view-certifications.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-certifications',
   templateUrl: './view-certifications.component.html',
-  styles: []
+  styles: ['li { max-width: 400px; margin: 14px auto; padding: 1px 0; border-radius: 3px;} li:hover { box-shadow: 1px 1px 4px black}']
 })
 export class ViewCertificationsComponent implements OnInit {
 
